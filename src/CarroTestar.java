53f70dec4977a42c0e76155f72f6e85c82daebda
gustgaspar <gustavogaspar248@gmail.com>
2024-10-15 18:17:02 -0300

    teste

---
+++ src/CarroTestar.java
@@ -0,0 +1,21 @@
+public class CarroTestar {
+    public static void main(String[] args) {
+
+        Carro c1 = new Carro();
+
+        c1.nome = "Uno";
+        c1.marca = "Fiat";
+        c1.ano = 2015;
+        c1.vel = 60;
+
+        c1.acelerar(10);
+
+        System.out.println("Velocidade: " + c1.vel + " km/h");
+
+        c1.freiar(30);
+
+        System.out.println("Velocidade: " + c1.vel + " km/h");
+
+        System.out.println(c1.nome + " " + c1.marca);
+    }
+}
